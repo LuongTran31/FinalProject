b79fa92163ee43ced8432d3a97efc4fcb0922ff3
LuongTran31 <tr.ducluong2000@gmail.com>
2023-04-13 03:02:26 +0700

    done some api message and product

--- api/routes/messageRoute.js
+++ api/routes/messageRoute.js
@@ -1,5 +1,8 @@
 import express from "express";
-
+import { verifyToken } from "../middleware/jwt.js";
+import { createMessage } from "../controllers/messageController.js";
 const router = express();
 
+router.post("/createMessage",verifyToken,createMessage)
+router.delete("/deleteMessage",verifyToken);
 export default router;
